9b15552d95a5fe16005df0d0aad8c3a27445d46c
pdmurray <peynmurray@gmail.com>
2022-01-09 22:53:35 -0800

    Added short bio

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,19 @@ export default function AboutPage(): JSX.Element {
     return (
         <main>
             <h1>Peyton Murray</h1>
+
+            Hi! I'm Peyton. Currently I work for <a href="https://www.quansight.com/">Quansight</a>
+            where I build open source software; sometimes I do contracting work through my company.
+            Before I was a programmer, I studied nanoscale magnetism as a postdoc at the <a
+                href="https://www.tuni.fi/en/about-us/computational-physics">Computational Physics
+            Laboratory</a> at Tampere University. At UC Davis I studied nanoscale magnetism from an
+            experimental perspective as part of the <a
+                href="http://kailiu.georgetown.domains/research/">Liu Lab</a> - which has since
+            moved to Georgetown University. In my free time I like to write software for science,
+            and am an active contributor to several open source projects. I'm driven by the desire
+            to make computational power more accessible to more people around the world - especially
+            in support of scientific research.
+
         </main>
     )
 }
