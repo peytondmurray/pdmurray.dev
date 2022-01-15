1d841fbbfae72eb3730860da9ad0a8be9cd04e98
pdmurray <peynmurray@gmail.com>
2022-01-15 11:57:42 -0800

    Added more icons for the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,39 +4,63 @@ import styled from 'styled-components'
 
 const links = [
     {
-        name: "peynmurray@gmail.com",
-        to: "email:peynmurray@gmail.com",
-        icon: ""
+        name: 'peynmurray@gmail.com',
+        to: 'email:peynmurray@gmail.com',
+        icon: (
+            <StaticImage src="../images/icons/at-solid.svg" alt="at symbol" />
+        ),
     },
     {
-        name: "GitHub",
-        to: "https://github.com/peytondmurray",
-        icon: <StaticImage src='../images/icons/github.svg' alt="GitHub logo" />
+        name: 'GitHub',
+        to: 'https://github.com/peytondmurray',
+        icon: (
+            <StaticImage src="../images/icons/github.svg" alt="GitHub logo" />
+        ),
     },
     {
-        name: "Linkedin",
-        to: "https://www.linkedin.com/in/peytondm/",
-        icon: "",
+        name: 'Linkedin',
+        to: 'https://www.linkedin.com/in/peytondm/',
+        icon: (
+            <StaticImage
+                src="../images/icons/linkedin.svg"
+                alt="LinkedIn logo"
+            />
+        ),
     },
     {
-        name: "ORCiD",
-        to: "https://orcid.org/0000-0003-0389-0611",
-        icon: ""
+        name: 'ORCiD',
+        to: 'https://orcid.org/0000-0003-0389-0611',
+        icon: <StaticImage src="../images/icons/orcid.svg" alt="ORCiD logo" />,
     },
     {
-        name: "Google Scholar",
-        to: "https://scholar.google.ca/citations?user=NXE8TDYAAAAJ&hl=en&oi=ao",
-        icon: "",
+        name: 'Google Scholar',
+        to: 'https://scholar.google.ca/citations?user=NXE8TDYAAAAJ&hl=en&oi=ao',
+        icon: (
+            <StaticImage
+                src="../images/icons/google-scholar.svg"
+                alt="Google Scholar logo"
+            />
+        ),
     },
     {
-        name: "ResearchGate",
-        to: "https://www.researchgate.net/profile/Peyton-Murray",
-        icon: ""
+        name: 'ResearchGate',
+        to: 'https://www.researchgate.net/profile/Peyton-Murray',
+        icon: (
+            <StaticImage
+                src="../images/icons/researchgate.svg"
+                alt="ResearchGate logo"
+            />
+        ),
     },
     {
-        name: "Stack Overflow",
-        to: "https://stackoverflow.com/users/8100451/peytondmurray",
-        icon: ""
+        name: 'Stack Overflow',
+        to: 'https://stackoverflow.com/users/8100451/peytondmurray',
+        icon: (
+            <StaticImage
+                src="../images/icons/stackoverflow.svg"
+                alt="Stack Overflow logo"
+            />
+        ),
     },
 ]
 
@@ -56,10 +80,11 @@ const SidebarLayout = styled.div`
 const IconTextLayout = styled.div`
     display: flex;
     flex-direction: row;
+    align-items: baseline;
 `
 
 const Name = styled.div`
-
+    margin: 0px 0px 5px 0px;
 `
 
 const Icon = styled.div`
@@ -70,7 +95,7 @@ const Icon = styled.div`
 export default function SidebarBase(): JSX.Element {
     return (
         <SidebarLayout>
-            {links.map(({name, to, icon}) => {
+            {links.map(({ name, to, icon }) => {
                 return (
                     <a href={to}>
                         <IconTextLayout key={name}>
