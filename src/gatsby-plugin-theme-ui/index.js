e5aa97cd47c4b3c2e571281e4dd35761f2a7e7a0
pdmurray <peynmurray@gmail.com>
2022-01-15 17:01:42 -0800

    Add cmocean.tempo as colorscheme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,50 @@
+// cmocean "tempo" colormap
+
+const colormap = [
+    '#FFF6F5',
+    '#F4EFE8',
+    '#E9E7DD',
+    '#DEE0D2',
+    '#D1D9C6',
+    '#C5D2BB',
+    '#B9CCB2',
+    '#ADC6A9',
+    '#9EC0A0',
+    '#91BA98',
+    '#83B592',
+    '#74B08D',
+    '#63A988',
+    '#53A484',
+    '#439E82',
+    '#339880',
+    '#23907F',
+    '#18897D',
+    '#12827B',
+    '#107A78',
+    '#137275',
+    '#166A71',
+    '#18636D',
+    '#1A5B69',
+    '#1B5364',
+    '#1C4B5F',
+    '#1C445B',
+    '#1B3D56',
+    '#193451',
+    '#182D4C',
+    '#162548',
+    '#151D43',
+]
+
 const theme = {
-  colors: {
-    text: '#000',
-    background: '#660099',
-  },
+    fonts: {
+        body: 'system-ui, sans-serif',
+        heading: '"Avenir Next", sans-serif',
+        monospace: 'Menlo, monospace',
+    },
+    colors: {
+        text: colormap[0],
+        background: colormap[31],
+    },
 }
 
 export default theme
