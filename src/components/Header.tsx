283916176dc3e0ee7f94c4312c3a0a2e9af60abc
pdmurray <peynmurray@gmail.com>
2022-01-16 11:45:23 -0800

    Minor styling edits

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,14 @@ import styled from '@emotion/styled'
 const HeaderLayout = styled.div`
     display: flex;
     flex-direction: row;
+    margin: 0em 0em 0em 5em;
 `
 
 export default function Header(): JSX.Element {
     return (
         <HeaderLayout>
             <Link to="/" as={GatsbyLink}>
-                <h1>Test</h1>
+                <h1>Peyton Murray</h1>
             </Link>
         </HeaderLayout>
     )
