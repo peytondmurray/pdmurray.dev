a7df4f111c323013f0cfdf54643b1d35aa8c2bd6
pdmurray <peynmurray@gmail.com>
2022-04-09 12:45:08 -0700

    "Just use HashRouter." Doesn't work for local dev...

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 import App from './App'
 
-const basename =
-    typeof import.meta.env.PUBLIC_URL !== 'undefined'
-        ? (import.meta.env.PUBLIC_URL as string)
-        : '/'
-
 ReactDOM.render(
     <React.StrictMode>
-        <Router basename={basename}>
+        <Router>
             <App />
         </Router>
     </React.StrictMode>,
