12fcc37bf388db0c8b7da36f44a3a154104edd58
pdmurray <peynmurray@gmail.com>
2022-06-03 22:50:16 -0700

    Added black hole, but stippled with d3!

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,74 +1,112 @@
+import { useRef, useEffect } from 'react'
 import { Heading } from '@chakra-ui/react'
 import Page from '../../components/Page'
 import ProjectsMdx from './projects.mdx'
 import components from '../../Theme/components'
 
+import BhCropped from './blackhole_cropped.png'
+
 import * as d3 from 'd3'
 
-async function getLuminanceData(image: string) {
-    const img = new Image()
-    img.src = image
+async function getImg(image: string) {
+  const img = new Image()
+  img.src = image
+  img.crossOrigin = 'Anonymous'
+  await img.decode()
+  return img
+}
+
+async function loadImage(image: string, canvas: HTMLCanvasElement): Promise<HTMLImageElement> {
+  let img!: HTMLImageElement
+  const loadPromise = new Promise(resolve => {
+    img = new Image()
+    img.onload = resolve
     img.crossOrigin = 'Anonymous'
-    await img.decode()
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
-    ctx?.drawImage(img, img.width, img.height)
-    const { data: rgba } = ctx!.getImageData(0, 0, img.width, img.height)
-    const data = {
-        width: img.width,
-        height: img.height,
-        values: new Float64Array(img.width * img.height),
-    }
-    rgba.filter((_, index) => index % 4 === 0).forEach((value, i) => {
-        data.values[i] = Math.max(0, 1 - value / 255)
-    })
-    return data
+    img.src = image
+  })
+  await loadPromise
+  return img
 }
 
-async function generateStippledImage(image: string) {
-    const imageData = await getLuminanceData(image)
-    const worker = new Worker(new URL('./worker.js', import.meta.url), {
-        type: 'module',
-    })
+async function getLuminanceData(image: string) {
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  const img = await loadImage(image, canvas)
+  const {width, height} = img
+  canvas.width = width
+  canvas.height = height
+  ctx?.drawImage(img, 0, 0, width, height)
+  const { data: rgba } = ctx!.getImageData(0, 0, width, height)
+  canvas.remove()
 
-    const canvas: any = d3.select('#container').append('canvas').node()
-    // .attr("width", imageData.width)
-    // .attr("height", imageData.height)
-    const context = canvas.getContext('2d')!
-
-    async function messaged({ data: { values, width, height } }: any) {
-        context.fillStyle = '#ffffff'
-        context.fillRect(0, 0, width, height)
-        context.beginPath()
-        const n = values.length
-        for (let i = 0; i < n; i += 2) {
-            const x = values[i]
-            const y = values[i + 1]
-            context.moveTo(x + 1.5, y)
-            context.arc(x, y, 1.5, 0, 2 * Math.PI)
-        }
-        context.fillStyle = '#000000'
-        context.fill()
-    }
-
-    worker.addEventListener('message', messaged)
-    worker.postMessage(imageData)
-    return context.canvas
+  const data = {
+    width: width,
+    height: height,
+    values: new Float64Array(width * height),
+  }
+  for (let i = 0; i < rgba.length / 4; i++) {
+    data.values[i] = Math.max(0, rgba[i*4] / 255)
+    // data.values[i] = Math.max(0, 1-rgba[i*4] / 255)
+  }
+  return data
 }
 
-// generateStippledImage('https://raw.githubusercontent.com/peytondmurray/bhsim/main/blackhole.png')
-generateStippledImage(
-    'https://static.observableusercontent.com/files/14959f050311f400368624031a7b9e4285f35c65ca4022f618f9250d7163ef4b0a0582de20f7d9790ed76b3442b4a77ebb96b86f641c1d8466f6544325144aed?response-content-disposition=attachment%3Bfilename*%3DUTF-8%27%27obama.png'
-)
+async function generateStippledImage(image: string, ref: React.MutableRefObject<any>) {
+  const imageData = await getLuminanceData(image)
+  const canvas: any = d3
+    .select(ref.current)
+    .attr('width', imageData.width)
+    .attr('height', imageData.height)
+    .node()
+  const context = canvas.getContext('2d')
+  const worker = new Worker(new URL('./worker.js', import.meta.url), {
+    type: 'module',
+  })
+
+
+  async function messaged({
+    data: { values, width, height }
+  }: {
+    data: {
+        values: Float64Array,
+        width: number,
+        height: number,
+    }
+  }) {
+    context.fillStyle = '#101010'
+    context.fillRect(0, 0, width, height)
+    context.beginPath()
+    const n = values.length
+    for (let i = 0; i < n; i += 2) {
+      const x = values[i]
+      const y = values[i + 1]
+      context.moveTo(x + 1.5, y)
+      context.arc(x, y, 1, 0, 2 * Math.PI)
+    }
+    context.fillStyle = '#ffffff'
+    context.fill()
+  }
+
+  worker.addEventListener('message', messaged)
+  worker.postMessage(imageData)
+}
 
 export default function Projects(): JSX.Element {
-    return (
-        <Page>
-            <Heading as="h2" size="xl">
-                Projects
-            </Heading>
-            <div id="container" />
-            <ProjectsMdx components={components} />
-        </Page>
-    )
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      generateStippledImage(BhCropped, canvasRef)
+    }
+  }, [canvasRef.current])
+
+  return (
+    <Page>
+      <Heading as="h2" size="xl">
+        Projects
+      </Heading>
+      <canvas id="canvas" ref={canvasRef} />
+      <ProjectsMdx components={components} />
+    </Page>
+  )
 }
--- src/pages/Projects/worker.js
+++ src/pages/Projects/worker.js
@@ -1,59 +1,59 @@
 import { Delaunay } from 'd3-delaunay'
 
 onmessage = ({ data: { values, width, height } }) => {
-    const n = values.length
-    const points = new Float64Array(n * 2)
-    const c = new Float64Array(n * 2)
-    const s = new Float64Array(n)
+  const n = Math.round(width*height / 20)
+  const points = new Float64Array(n * 2)
+  const c = new Float64Array(n * 2)  // (x0, y0, x1, y1, ...)
+  const s = new Float64Array(n)  // Voronoi cell weights
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < 30; j++) {
-            const x = (points[i * 2] = Math.floor(Math.random() * width))
-            const y = (points[i * 2 + 1] = Math.floor(Math.random() * height))
+  // Carry out rejection sampling to randomly place stipples with greater density at the the darkest
+  // parts of the image
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < 130; j++) {
+      const x = (points[i * 2] = Math.floor(Math.random() * width))
+      const y = (points[i * 2 + 1] = Math.floor(Math.random() * height))
 
-            if (Math.random() < values[y * width + x]) {
-                break
-            }
-        }
+      if (Math.random() < values[y * width + x]) {
+        break
+      }
+    }
+  }
+
+  const delaunay = new Delaunay(values)
+  const voronoi = delaunay.voronoi([0, 0, width, height])
+
+  for (let k = 0; k < 30; k++) {
+    //Compute the weighted centroid for each Voronoi cell
+    c.fill(0)
+    s.fill(0)
+    let i = 0
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const w = values[y*width + x]  // weight - given by brightness of pixel at (x, y)
+        i = delaunay.find(x+0.5, y+0.5, i)  // find the voronoi cell nearest to (x, y)
+        s[i] += w  // Keep a sum of the weights added for each voronoi cell
+        c[i * 2] += w*(x+0.5)  //  x-voronoi cell weights?
+        c[i*2 + 1] += w*(y+0.5)  // y-voronoi cell weights?
+      }
     }
 
-    // const delaunay = new Delaunay(values)
-    // const voronoi = delaunay.voronoi([0, 0, width, height])
+    // Relax the diagram by moving points to the weighted centroid.
+    // Wiggle the points a little bit so they don't get stuck.
+    const w = Math.pow(k + 1, -0.8)*10
+    for (let i = 0; i < n; i++) {
+      const x0 = points[i*2]
+      const y0 = points[i*2+1]
 
-    postMessage({ values: points, width, height })
+      const x1 = s[i] ? c[i*2]/s[i] : x0
+      const y1 = s[i] ? c[i*2+1]/s[i] : y0
 
-    // for (let k = 0; k < 80; k++) {
-    //   //Compute the weighted centroid for each Voronoi cell
-    //   c.fill(0)
-    //   s.fill(0)
-    //   let i = 0
-    //   for (let y = 0; y < height; y++) {
-    //     for (let x = 0; x < width; x++) {
-    //       const w = values[y*width + x]
-    //       i = delaunay.find(x+0.5, y+0.5, i)
-    //       s[i] += w
-    //       c[i * 2] += w*(x+0.5)
-    //       c[i*2 + 1] += w*(y+0.5)
-    //     }
-    //   }
+      points[i*2] = x0 +  (x1 - x0)*1.8 + (Math.random() - 0.5)*w
+      points[i*2+1] = y0 +  (y1 - y0)*1.8 + (Math.random() - 0.5)*w
+    }
 
-    //   // Relax the diagram by moving points to the weighted centroid.
-    //   // Wiggle the points a little bit so they don't get stuck.
-    //   const w = Math.pow(k + 1, -0.8)*10
-    //   for (let i = 0; i < n; i++) {
-    //     const x0 = points[i*2]
-    //     const y0 = points[i*2+1]
+    postMessage({values: points, width, height})
+    voronoi.update()
+  }
 
-    //     const x1 = s[i] ? c[i*2]/s[i] : x0
-    //     const y1 = s[i] ? c[i*2+1]/s[i] : y0
-
-    //     points[i*2] = x0 +  (x1 - x0)*1.8 + (Math.random() - 0.5)*w
-    //     points[i*2+1] = y0 +  (y1 - y0)*1.8 + (Math.random() - 0.5)*w
-    //   }
-
-    //   postMessage({values: points, width, height})
-    //   voronoi.update()
-    // }
-
-    close()
+  close()
 }
