d3e3bfcf44c17625ea5e7d2cc88a552a41178b79
pdmurray <peynmurray@gmail.com>
2022-12-29 23:12:25 -0800

    Start fresh; let's go to 11ty!

--- src/components/CenteredImage.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Image } from '@chakra-ui/react'
-
-export default function CenterImage(props: any): JSX.Element {
-  return (
-    <Center>
-      <Image {...props} />
-    </Center>
-  )
-}
